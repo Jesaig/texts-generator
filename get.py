b160840d9c0a356f1f8cf85a41ea0bc2854821c0
Айгуль Аданисовна Газимова <gazimova.aa@phystech.edu>
2018-03-26 21:16:50 +0000

    Update get.py

--- get.py
+++ get.py
@@ -2,10 +2,12 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir', nargs='?', type=argparse.FileType('r', encoding="utf-8"),
+parser.add_argument('--input-dir', nargs='?',
+                    type=argparse.FileType('r', encoding="utf-8"),
                     default=open('input.txt', 'r', encoding="utf-8"),
                     help='Input file')
-parser.add_argument('--model', nargs='?', type=argparse.FileType('w', encoding="utf-8"),
+parser.add_argument('--model', nargs='?',
+                    type=argparse.FileType('w', encoding="utf-8"),
                     default=sys.stdout,
                     help='Output file')
 parser.add_argument('--lc', nargs='?', const=True, default=False,
