b2cc6ca05fe35a2e706e6ebc02064e92cbbd7a6a
Айгуль Аданисовна Газимова <gazimova.aa@phystech.edu>
2018-03-26 21:00:16 +0000

    Add new file

---
+++ get.py
@@ -0,0 +1,19 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-dir', nargs='?', type=argparse.FileType('r', encoding="utf-8"),
+                    default=open('input.txt', 'r', encoding="utf-8"),
+                    help='Input file')
+parser.add_argument('--model', nargs='?', type=argparse.FileType('w', encoding="utf-8"),
+                    default=sys.stdout,
+                    help='Output file')
+parser.add_argument('--lc', nargs='?', const=True, default=False,
+                    help='To lowercase')
+
+
+args = parser.parse_args()
+
+ins = vars(args).get('input_dir')
+outs = vars(args).get('model')
+lc = vars(args).get('lc')
