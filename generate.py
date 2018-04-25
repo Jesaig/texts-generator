c5a6ec31c8777d87b473e97b1cd1c673e9d632b1
Айгуль Аданисовна Газимова <gazimova.aa@phystech.edu>
2018-04-25 01:14:58 +0000

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,7 +2,6 @@ import numpy as np
 import argparse
 import sys
 import json
-from pprint import pprint
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', nargs='?',
@@ -15,11 +14,11 @@ parser.add_argument('--output', nargs='?',
                     help='Output file')
 parser.add_argument('--length', nargs='?',
                     type=int,
-                    default=200,
+                    default=100,
                     help='Generated text length (in words)')
 parser.add_argument('--seed', nargs='?',
                     type=str,
-                    default=-1,
+                    default=None,
                     help='First word')
 
 args = parser.parse_args()
@@ -27,30 +26,34 @@ args = parser.parse_args()
 ins = vars(args).get('model')
 outs = vars(args).get('output')
 
-data = json.load(ins)
-wordsList = data[0]
-wordsFrequency = data[1]
-newWords = data[2]
 
+class ModelGenerator:
+    def __init__(self):
+        data = json.load(ins)
+        self.wordsList = data[0]
+        self.wordsFrequency = data[1]
+        self.newWords = data[2]
 
-def getFirstWord():
-    return np.random.choice(wordsList, 1, wordsFrequency)[0]
+    def get_first_word(self):
+        return np.random.choice(self.wordsList, 1, self.wordsFrequency)[0]
 
+    def get_word(self, word=-1):
+        if word not in self.newWords or len(self.newWords[word][0]) == 0:
+            return self.get_first_word()
+        return np.random.choice(self.newWords[word][0], 1,
+                                self.newWords[word][1])[0]
 
-def getWord(word=-1):
-    if word not in newWords or newWords[word][0].__len__ == 0:
-        return getFirstWord()
-    return np.random.choice(newWords[word][0], 1, newWords[word][1])[0]
-
-
-def genText(len, firstWord):
-    if (firstWord != -1):
-        len -= 1
-        outs.write("%s " % firstWord)
-    word = getWord(firstWord)
-    outs.write("%s " % word)
-    for i in range(len - 1):
-        word = getWord(word)
+    def gen_text(self, length, first_word):
+        if first_word is not None:
+            length -= 1
+            outs.write("%s " % first_word)
+        word = self.get_word(first_word)
         outs.write("%s " % word)
+        for i in range(length - 1):
+            word = self.get_word(word)
+            outs.write("%s " % word)
 
-genText(vars(args).get('length'), vars(args).get('seed'))
+
+if __name__ == "__main__":
+    ModelGenerator().gen_text(vars(args).get('length'),
+                              vars(args).get('seed'))
